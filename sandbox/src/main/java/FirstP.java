62900973808e6cad03bd3ade57b9258a9d54799a
d.karazanova <d.karazanova@solarsecurity.ru>
2018-10-27 00:49:17 +0300

    Distance_lesson1

--- sandbox/src/main/java/FirstP.java
+++ sandbox/src/main/java/FirstP.java
@@ -1,5 +1,13 @@
 public class FirstP{
     public static void main(String[]arg){
-System.out.println("May");
+
+        Point p1 = new Point(5, 2);
+        Point p2 = new Point(10, 2);
+        System.out.println("rasstoyanie: " + length(p1, p2));
+        System.out.println("rasstoyanie: " + Point.distance(p1, p2));
 	}
+    public static double length(Point p1, Point p2) {
+
+        return Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+    }
 }
